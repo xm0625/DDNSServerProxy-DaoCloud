9f1cfaea483cf41a977e091f357b2e3f80db32d4
xm <402276694@qq.com>
2017-09-27 16:21:36 +0800

    更新DaoCloudServer的定时重启机制, 改为重新部署最新版本的镜像

--- appWatcher.py
+++ appWatcher.py
@@ -17,12 +17,58 @@ import urllib,urllib2
 import json
 
 
+# Deprecated 概率性重启失败
 def restart_app(api_token, app_id):
     url = "https://openapi.daocloud.io/v1/apps/" + app_id + "/actions/restart"
-    req = urllib2.Request(url=url, data={}, headers={"Authorization": "token "+api_token})
+    req = urllib2.Request(url=url, data=urllib.urlencode({}), headers={"Authorization": "token "+api_token})
     res_data = urllib2.urlopen(req)
     res = res_data.read()
     print res
+
+
+def stop_app(api_token, app_id):
+    url = "https://openapi.daocloud.io/v1/apps/" + app_id + "/actions/stop"
+    req = urllib2.Request(url=url, data=urllib.urlencode({}), headers={"Authorization": "token "+api_token})
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    print res
+
+
+def start_app(api_token, app_id):
+    url = "https://openapi.daocloud.io/v1/apps/" + app_id + "/actions/start"
+    req = urllib2.Request(url=url, data=urllib.urlencode({}), headers={"Authorization": "token "+api_token})
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    print res
+
+
+def get_action_detail(api_token, app_id, action_id):
+    url = "https://openapi.daocloud.io/v1/apps/" + app_id + "/actions/" + action_id
+    req = urllib2.Request(url=url, headers={"Authorization": "token "+api_token})
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    print res
+
+
+def redeploy_app(api_token, app_id, release_name=None):
+    if release_name is None:
+        release_name = get_app_detail(api_token, app_id)["release_name"]
+    print release_name
+    url = "https://openapi.daocloud.io/v1/apps/" + app_id + "/actions/redeploy"
+    req = urllib2.Request(url=url, data=json.dumps({"release_name": str(release_name)}), headers={"Authorization": "token "+api_token})
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    print res
+
+
+def get_app_detail(api_token, app_id):
+    url = "https://openapi.daocloud.io/v1/apps/" + app_id
+    req = urllib2.Request(url=url, headers={"Authorization": "token " + api_token})
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    print res
+    return json.loads(res)
+
 
 if __name__ == '__main__':
     if "API_TOKEN" not in os.environ:
@@ -30,5 +76,6 @@ if __name__ == '__main__':
     if "APP_ID" not in os.environ:
         exit(1)
     time.sleep(10 * 3600)
-    restart_app(os.environ["API_TOKEN"], os.environ["APP_ID"])
+    redeploy_app(os.environ["API_TOKEN"], os.environ["APP_ID"])
+
 
