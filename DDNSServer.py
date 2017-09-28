13d9ee9b9ab58c25ade7e84ee80bb577dd9b9321
xm <402276694@qq.com>
2017-09-28 09:57:41 +0800

    注释更新

--- DDNSServer.py
+++ DDNSServer.py
@@ -17,6 +17,7 @@ import json
 import base64
 
 
+# 通用异常处理
 class CommonException(Exception):
     code = "0"
     message = "system busy"
@@ -29,6 +30,7 @@ class CommonException(Exception):
             self.message = message
 
 
+# 调用3322 API接口
 def perform_dns_update(hostname, user, password, ip):
     url = "http://members.3322.net/dyndns/update?system=dyndns&hostname=%s&myip=%s" % (hostname, ip)
     request = urllib2.Request(url)
@@ -44,6 +46,7 @@ def perform_dns_update(hostname, user, password, ip):
         return {"httpCode": "-1", "reason": e.reason}
 
 
+# 路径为"/"的控制逻辑方法(MVC中的C)
 def parse_and_fetch(request, remote_address):
     if "hostname" not in request.keys():
         raise CommonException("-1", "hostname not exist")
@@ -54,6 +57,7 @@ def parse_and_fetch(request, remote_address):
     return perform_dns_update(request["hostname"], request["user"], request["password"], remote_address)
 
 
+# http server 主逻辑, 这里是一个简陋的路由
 def app(environ, start_response):
     request_method = environ["REQUEST_METHOD"] #GET
     path_info = environ["PATH_INFO"]  # /hi/name/index.action
@@ -104,7 +108,7 @@ def app(environ, start_response):
     start_response(response_code,response_header)
     return [response_string]
 
-# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+# 创建一个服务器，IP地址为空，端口是8090，处理函数是app:
 httpd = make_server('', 8090, app)
 print "Serving HTTP on port 8090..."
 # 开始监听HTTP请求:
